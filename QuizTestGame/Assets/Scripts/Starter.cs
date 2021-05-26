d14c302f079b7e597e674abb2eca167c887e5914
cyberaven <cyberaven@yandex.ru>
2021-05-26 19:04:12 +0300

    frstComm
    frstComm

---
+++ QuizTestGame/Assets/Scripts/Starter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    [SerializeField] private StateMashine stateMashine;
+
+    private void Awake()
+    {
+        stateMashine = Instantiate(stateMashine);
+    }
+}
